9227edfb26cace7e747869476ed867cf2f3a3355
Sarchwalk <sarah.walker45@gmail.com>
2018-06-06 11:36:25 -0600

    Add files via upload

---
+++ webScraper V1/runScrape.py
@@ -0,0 +1,13 @@
+from webScraper import Parser
+from fileIO import fileHandler
+from intelligentParser import Summarizer
+
+p = Parser()
+teamInfo = p.getData('2008')
+
+summarizer = Summarizer()
+summary = ''
+
+for i in range(len(teamInfo)):
+	teamInfo[i][1] = summarizer.summarize(teamInfo[i][1]) 
+
